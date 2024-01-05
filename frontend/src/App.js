cdaf676a4ff3c725b184f425c11e345437f8fd9a
linarawwas <linamkraw2@gmail.com>
2024-01-05 15:48:31 +0200

    added layout successfully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,13 @@
 import "./App.css";
-import { Navigate, Route, Routes, Router } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Layout from '../src/Layout/Layout.jsx'
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={<Layout />} />
+        <Route path="/*" element={<Layout />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   )
 }
 
